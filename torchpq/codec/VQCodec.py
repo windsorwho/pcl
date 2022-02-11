e22b3ee4db9642d723d506755c2f550a417ca229
windsor <94354961+windsorhwu@users.noreply.github.com>
2022-02-11 21:49:03 +0800

    yapf on py files.

--- torchpq/codec/VQCodec.py
+++ torchpq/codec/VQCodec.py
@@ -1,23 +1,20 @@
 import torch
-
 from ..clustering import KMeans
 # from ..clustering import KMeansOld as KMeans
 from .BaseCodec import BaseCodec
 
+
 class VQCodec(BaseCodec):
-  def __init__(self, *args, **kwargs):
-    super(VQCodec, self).__init__()
-    self.kmeans = KMeans(
-      *args,
-      **kwargs
-    )
+    def __init__(self, *args, **kwargs):
+        super(VQCodec, self).__init__()
+        self.kmeans = KMeans(*args, **kwargs)
 
-  @property
-  def codebook(self):
-    return self.kmeans.centroids
+    @property
+    def codebook(self):
+        return self.kmeans.centroids
 
-  def encode(self, input):
-    """
+    def encode(self, input):
+        """
       input:
         torch.Tensor
         shape : [d_vector, n_data]
@@ -28,11 +25,11 @@ class VQCodec(BaseCodec):
         shape : [n_data]
         dtype : int64
     """
-    assert self._is_trained == True, "codec is not trained"
-    return self.kmeans.predict(input)
+        assert self._is_trained == True, "codec is not trained"
+        return self.kmeans.predict(input)
 
-  def decode(self, code):
-    """
+    def decode(self, code):
+        """
       code:
         torch.Tensor
         shape : [n_code]
@@ -43,16 +40,16 @@ class VQCodec(BaseCodec):
         shape : [d_vector, n_data]
         dtype : float32
     """
-    assert self._is_trained == True, "Codec is untrained"
-    return self.codebook[:, code].clone()
+        assert self._is_trained == True, "Codec is untrained"
+        return self.codebook[:, code].clone()
 
-  def train(self, data):
-    """
+    def train(self, data):
+        """
       data:
         torch.Tensor
         shape : [d_vector, n_data]
         dtype : float32
     """
-    labels = self.kmeans.fit(data)
-    self._trained(True)
-    return labels
+        labels = self.kmeans.fit(data)
+        self._trained(True)
+        return labels
