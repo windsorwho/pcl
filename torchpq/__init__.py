a658e8410b2158287db0321b2fdf45196bda13d9
windsor <94354961+windsorhwu@users.noreply.github.com>
2022-02-11 21:31:53 +0800

    shamelessly add torchpq into source tree

---
+++ torchpq/__init__.py
@@ -0,0 +1,21 @@
+import torch
+try:
+  import cupy as cp
+except ModuleNotFoundError:
+  raise ModuleNotFoundError("cupy is not installed, please visit https://pypi.org/project/cupy/")
+
+from . import container
+from . import codec
+from . import transform
+from . import clustering
+from . import kernels
+from . import experimental
+from . import metric
+from . import util
+from . import legacy
+from . import index
+from . import fn
+
+from .CustomModule import CustomModule
+
+topk = fn.Topk()
---
+++ torchpq/kernels/__init__.py
@@ -0,0 +1,25 @@
+from .CustomKernel import CustomKernel
+from .CustomKernel import Stream
+
+from .GetAddressByIDCuda import GetAddressByIDCuda
+from .GetDivByAddressCuda import GetDivByAddressCuda
+from .GetDivByAddressV2Cuda import GetDivByAddressV2Cuda
+from .GetIOACuda import GetIOACuda
+from .GetWriteAddressCuda import GetWriteAddressCuda
+from .GetWriteAddressV2Cuda import GetWriteAddressV2Cuda
+from .PQDecodeCuda import PQDecodeCuda
+from .ComputeCentroidsCuda import ComputeCentroidsCuda
+from .ComputeProductCuda import ComputeProductCuda
+from .MaxSimCuda import MaxSimCuda
+
+from .BMMCuda import BMMCuda
+from .MinBMMCuda import MinBMMCuda
+from .TopkBMMCuda import TopkBMMCuda
+
+from .IVFPQTopkCuda import IVFPQTopkCuda
+from .IVFPQTop1Cuda import IVFPQTop1Cuda
+from .DistributedIVFPQTopkCuda import DistributedIVFPQTopkCuda
+from .DistributedIVFPQTop1Cuda import DistributedIVFPQTop1Cuda
+from .TopkSelectCuda import TopkSelectCuda
+from .Top32SelectCuda import Top32SelectCuda
+from .Top1SelectCuda import Top1SelectCuda
