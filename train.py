946b377b6b31dd1138a76bba970d8212f62fec20
windsor <94354961+windsorhwu@users.noreply.github.com>
2022-02-14 08:38:04 +0800

    First deep learning based script.

--- train.py
+++ train.py
@@ -6,41 +6,41 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-#import torch.backends.cudnn as cudnn
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
 import time
-import os
-import copy
+from torch.utils.tensorboard import SummaryWriter
 
 import pcl_data
+import reid_model
 
 FEATURE_FILENAME = '/Users/wenzehu/data/pcl/train/dense_data.npz'
-
-pcl_dataset = pcl_data(FEATURE_FILENAME)
+FEATURE_FILENAME = '/data/pcl/train/dense_data.npz'
+pcl_dataset = pcl_data.PCLTrainDataset(FEATURE_FILENAME)
 dataloaders = {
     x: torch.utils.data.DataLoader(pcl_dataset,
-                                   batch_size=4,
+                                   batch_size=512,
                                    shuffle=True,
-                                   num_workers=4)
+                                   num_workers=1)
     for x in ['train', 'val']
 }
 dataset_sizes = {x: pcl_dataset.__len__() for x in ['train', 'val']}
-
+writer = SummaryWriter()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # To verify the dataset is loadable.
 inputs, classes = next(iter(dataloaders['train']))
 
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+def train_model(model,
+                criterion,
+                optimizer,
+                scheduler,
+                writer=None,
+                num_epochs=25):
     since = time.time()
 
     #    best_model_wts = copy.deepcopy(model.state_dict())
     #    best_acc = 0.0
-
+    num_itr = 0
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
@@ -78,29 +78,33 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
+                #if writer is not None:
+                if num_itr % 100 == 0 and writer is not None and phase == 'train':
+                    writer.add_scalar('LR',
+                                      scheduler.get_last_lr()[0], num_itr)
+                    writer.add_scalar('Loss/Train',
+                                      loss.item() * inputs.size(0), num_itr)
+                    writer.add_scalar(
+                        'Acc/Train',
+                        torch.sum(preds == labels.data) / inputs.size(0),
+                        num_itr)
+                num_itr += 1
             if phase == 'train':
                 scheduler.step()
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            writer.add_scalar('Loss/Train_epoc', epoch_loss, num_itr)
+            writer.add_scalar('Acc/Train_epoch', epoch_acc, num_itr)
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss,
                                                        epoch_acc))
-
-            # deep copy the model
-            #if phase == 'val' and epoch_acc > best_acc:
-            #    best_acc = epoch_acc
-            #    best_model_wts = copy.deepcopy(model.state_dict())
-
         print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    #print('Best val Acc: {:4f}'.format(best_acc))
 
-    # load best model weights
-    model.load_state_dict(best_model_wts)
     return model
 
 
@@ -110,19 +114,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 #
 # Load a pretrained model and reset final fully connected layer.
 #
-
-model_ft = models.resnet18(pretrained=True)
-num_ftrs = model_ft.fc.in_features
-# Here the size of each output sample is set to 2.
-# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-model_ft.fc = nn.Linear(num_ftrs, 2)
-
+model_ft = reid_model.ReIDModel(128, 15000)
 model_ft = model_ft.to(device)
 
 criterion = nn.CrossEntropyLoss()
+output = model_ft(inputs)
+writer.add_graph(model_ft, inputs)
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.1, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
@@ -139,72 +139,7 @@ model_ft = train_model(model_ft,
                        criterion,
                        optimizer_ft,
                        exp_lr_scheduler,
-                       num_epochs=25)
-
+                       writer=writer,
+                       num_epochs=100)
+writer.close()
 ######################################################################
-#
-
-visualize_model(model_ft)
-
-######################################################################
-# ConvNet as fixed feature extractor
-# ----------------------------------
-#
-# Here, we need to freeze all the network except the final layer. We need
-# to set ``requires_grad = False`` to freeze the parameters so that the
-# gradients are not computed in ``backward()``.
-#
-# You can read more about this in the documentation
-# `here <https://pytorch.org/docs/notes/autograd.html#excluding-subgraphs-from-backward>`__.
-#
-
-model_conv = torchvision.models.resnet18(pretrained=True)
-for param in model_conv.parameters():
-    param.requires_grad = False
-
-# Parameters of newly constructed modules have requires_grad=True by default
-num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, 2)
-
-model_conv = model_conv.to(device)
-
-criterion = nn.CrossEntropyLoss()
-
-# Observe that only parameters of final layer are being optimized as
-# opposed to before.
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
-
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
-
-######################################################################
-# Train and evaluate
-# ^^^^^^^^^^^^^^^^^^
-#
-# On CPU this will take about half the time compared to previous scenario.
-# This is expected as gradients don't need to be computed for most of the
-# network. However, forward does need to be computed.
-#
-
-model_conv = train_model(model_conv,
-                         criterion,
-                         optimizer_conv,
-                         exp_lr_scheduler,
-                         num_epochs=25)
-
-######################################################################
-#
-
-visualize_model(model_conv)
-
-plt.ioff()
-plt.show()
-
-######################################################################
-# Further Learning
-# -----------------
-#
-# If you would like to learn more about the applications of transfer learning,
-# checkout our `Quantized Transfer Learning for Computer Vision Tutorial <https://pytorch.org/tutorials/intermediate/quantized_transfer_learning_tutorial.html>`_.
-#
-#
